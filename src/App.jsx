3fad73641087542f882b90ae071e59cd59e08a3a
ram <stharamkumar3303@gmail.com>
2023-08-02 18:41:53 +0545

    pages and basic routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Users from './pages/Users'
 
 const App = () => {
   return (
-    <div>Users</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/all-users' element={<Users />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
